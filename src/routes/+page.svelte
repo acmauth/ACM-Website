a3371d356b1c2025c0a46d1890ef917cec8bf145
Christos Balaktsis <balaktsis@csd.auth.gr>
2025-04-22 18:48:32 +0300

    Example code

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<h1 class="title">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<a href="/about">About</a>
+
+<style>
+    .title {
+        background-color: aqua;
+    }
+</style>
