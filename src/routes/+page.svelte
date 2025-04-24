8672ba4129a7a57636382b5aed0d47704fb9bfdc
Christos Balaktsis <balaktsis@csd.auth.gr>
2025-04-24 17:30:21 +0300

    Dummy 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <h1 class="title">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<a href="/about">About dfbjbvsnsvsvs </a>
+<a href="/about">About dfbjbvsnsvsvs!!!! </a>
 
 <style>
     .title {
