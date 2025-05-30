1b52bbe9d94e72dbf34c46ac26ae816d33b1e262
nikos <nikoseuaggelou360@gmail.com>
2025-05-30 11:47:51 +0300

    background appearence changes

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -15,20 +15,17 @@
 
     {#if upcomingEvents.length!==0}
         <h2 class="component-header">Upcoming Events</h2>
-        <div class = "container">
-            <div class="carousel slide carousel-fade container position-relative" id="upcomingEventsCarousel"  data-bs-ride="carousel" >
+        <div class="carousel slide carousel-fade" id="upcomingEventsCarousel"  data-bs-ride="carousel" >
             <div class="carousel-inner">
                 {#each upcomingEvents as upComingEvent ,index}
-                <div class="carousel-item {index===0? 'active': ''}">
-                    <EventCard 
-                    name={upComingEvent.name}
-                    image={upComingEvent.image}
-                    date={upComingEvent.date}
-                    id={upComingEvent.id}
-                    />
-                     
-                   
-                </div>
+                    <div class="carousel-item {index===0? 'active': ''}">
+                        <EventCard 
+                        name={upComingEvent.name}
+                        image={upComingEvent.image}
+                        date={upComingEvent.date}
+                        id={upComingEvent.id}
+                        />
+                    </div>
                 {/each}
             </div>
 
@@ -40,54 +37,77 @@
             <span class="carousel-control-next-icon text-dark" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
             </button>
-        </div>
-        </div>
-        
+        </div>  
     {/if}
    
     <h2 class="component-header">Past Events</h2>
-    <div class="container py-4">
-            <Timeline {events} />
-        
+    <div class="container">
+        <Timeline {events} />
     </div>
 </div>
 
 <style>
     .page {
         min-height: 100vh;
-        background-image: url('../../lib/assets/blue-white-background.jpg');
-        background-size: cover;
-        background-position: center;
         padding-top: 3rem;
         padding-bottom: 3rem;
+        padding-left: 0.5rem;
+        padding-right: 0.5rem;
         text-align: center;
         width: 100%;
-        background-color: var(--bs-body-bg-secondary);
+        filter: contrast(1.1);
+        position: relative;
+
+        background-image:
+            linear-gradient(
+                to bottom right,
+                rgba(0, 77, 128, 0.6),
+                rgba(218, 241, 252, 0.6)
+                ),
+            url('../../lib/assets/blue-white-background.jpg');
+        background-size: cover;
+        background-position: center;
+        background-blend-mode: overlay;
+        background-attachment: fixed;
+    }
+    .page::before {
+        content: "";
+        position: absolute;
+        inset: 0;
+        pointer-events: none;
+        background: radial-gradient(
+            ellipse at center,
+            rgba(0,0,0,0) 70%,
+            rgba(0,0,0,0.4) 130%
+        );
+        mix-blend-mode: multiply;
+    }
+    .title, 
+    .component-header {
+        color: #004d80;
+        text-shadow: 1px 1px 2px rgba(0,0,0,0.1);
+        text-shadow: #daf1fc 2px 2px 4px;
     }
     .title {
         font-size: 3rem;
         font-weight: 800;
-        color: #004d80;
-        text-shadow: 1px 1px 2px rgba(0,0,0,0.1);
-        text-shadow: #daf1fc 2px 2px 4px;
-        margin-bottom: 4rem;
+    }
+    .component-header {
+        font-size: 2.5rem;
+        font-weight: 700;
+        margin-bottom: 3rem;
+        margin-top: 7rem;
     }
     .carousel {
-        margin-top: 5rem;
         border-radius: 1rem;
         padding: 0rem;
         width: 80%;              
         max-width: 1300px;     
         margin-left: auto;
         margin-right: auto;
-    }
-    .component-header {
-        font-size: 2.5rem;
-        font-weight: 700;
-        color: #004d80;
-        text-shadow: 1px 1px 2px rgba(0,0,0,0.1);
-        text-shadow: #daf1fc 2px 2px 4px;
-        margin: 3rem;
+        width: 70%;
+        position: relative;
+        
     }
     .carousel-inner {
         border-radius: 16px;
@@ -118,4 +138,5 @@
     .carousel-control-prev:hover, .carousel-control-next:hover {
         border-color: rgba(255, 255, 255, 1);
     }
+
 </style>
